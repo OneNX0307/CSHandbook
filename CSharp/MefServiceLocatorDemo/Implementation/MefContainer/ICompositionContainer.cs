357548beb29a723e5b729c7d752363b7aaca4bd3
xingyafei <xingyafei0801@outlook.com>
2020-03-18 14:09:50 +0800

    Add mef servicelocator code.

---
+++ CSharp/MefServiceLocatorDemo/Implementation/MefContainer/ICompositionContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MefContainer
+{
+	public interface ICompositionContainer
+	{
+		IEnumerable<object> GetAllInstances(Type type);
+		IEnumerable<T> GetAllInstances<T>();
+
+		object GetInstance(Type type);
+		T GetInstance<T>();
+		T GetInstance<T>(string key);
+		object GetInstance(Type type, string key);
+	}
+}
---
+++ CSharp/MefServiceLocatorDemo/Implementation/MefContainer/MefCompositionContainer.cs
@@ -0,0 +1,64 @@
+﻿using CommonServiceLocator;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MefContainer
+{
+	public class MefCompositionContainer : ICompositionContainer
+	{
+		private CompositionContainer container;
+
+		public MefCompositionContainer(ComposablePartCatalog catalog)
+		{
+			this.container = new CompositionContainer(catalog);
+		}
+		public IEnumerable<object> GetAllInstances(Type type)
+		{
+			var contract = AttributedModelServices.GetContractName(type);
+			return this.container.GetExportedValues<object>(contract);
+		}
+
+		public IEnumerable<T> GetAllInstances<T>()
+		{
+			return this.container.GetExportedValues<T>();
+		}
+
+		public object GetInstance(Type type)
+		{
+			var contract = AttributedModelServices.GetContractName(type);
+			return this.container.GetExports(type, null, contract).FirstOrDefault();
+		}
+
+		public T GetInstance<T>()
+		{
+			return this.container.GetExportedValue<T>();
+		}
+
+		public T GetInstance<T>(string key)
+		{
+			return this.container.GetExportedValue<T>(key);
+		}
+
+		public object GetInstance(Type type, string key)
+		{
+			var contract = AttributedModelServices.GetContractName(type);
+			return this.container.GetExports(type, null, contract).FirstOrDefault().Value;
+		}
+
+		public void Config()
+		{
+			this.container.ComposeParts(this);
+			ServiceLocatorAdapter serviceLocatorAdapter = new ServiceLocatorAdapter(this);
+			ServiceLocator.SetLocatorProvider(() => serviceLocatorAdapter);
+			this.container.ComposeExportedValue<IServiceLocator>(serviceLocatorAdapter);
+			this.container.ComposeExportedValue<MefCompositionContainer>(this);
+			this.container.ComposeExportedValue<ICompositionContainer>(this);
+		}
+	}
+}
